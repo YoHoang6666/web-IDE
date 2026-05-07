934526b1d8ec7f7ef19c0b913ec211b2a7a8ff88
YoHoang6666 <huy031855@stu.vinschool.edu.vn>
2026-05-07 13:55:13 +0000

    Fix QtDeploy syntax

--- cmake/QtDeploy.cmake
+++ cmake/QtDeploy.cmake
@@ -1,13 +1,12 @@
 function(webide_deploy_qt TARGET_NAME)
 
-```
 if(WIN32)
-    add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
+    add_custom_command(
+        TARGET ${TARGET_NAME}
+        POST_BUILD
         COMMAND windeployqt
-        $<TARGET_FILE:${TARGET_NAME}>
+                $<TARGET_FILE:${TARGET_NAME}>
     )
 endif()
-```
 
 endfunction()
-
